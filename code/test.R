401cae839f69a35efd5c24f5509c1aea8972c31d
xqnwang <wang_study1995@163.com>
2023-03-21 01:47:25 +1100

    Simple example to use the mip_l0 function

--- code/test.R
+++ code/test.R
@@ -1,4 +1,6 @@
 #----------------------------------------------------------------------
+# Comparison with L0Group
+#----------------------------------------------------------------------
 # import numpy as np
 # import scipy as sc
 # import math
@@ -14,7 +16,6 @@
 # Gsize = 10 # Group size.
 # 
 # x, y, group_indices, true_coefs = gen_synthetic(rho,n,p,k0,SNR,Gsize)
-#----------------------------------------------------------------------
 
 library(dplyr)
 source('R/sourceDir.R')
@@ -34,5 +35,63 @@ model <- mip_l0l2(x = x, y = y, group_indices = group_indices,
 
 b <- model$coefficients
 z <- model$binary
+
+
+#----------------------------------------------------------------------
+# Subset selection with shrinkage under unbiasedness assumptions
+#----------------------------------------------------------------------
+library(fable)
+library(dplyr)
+library(tidyr)
+library(tsibble)
+library(tsibbledata)
+library(lubridate)
+library(purrr)
+library(ggplot2)
+
+source('R/sourceDir.R')
+sourceDir("R", recursive = TRUE)
+
+fc <- c(10, 4, 4, 2, 4, 2, 2)
+S <- rbind(rep(1, 4),
+           c(1, 1, 0, 0), c(0, 0, 1, 1),
+           diag(1, 4))
+W <- diag(7)
+G_bu <- cbind(rep(0, 4), rep(0, 4), rep(0, 4), diag(1, 4))
+G_td <- cbind(c(0.2, 0.4, 0.2, 0.2), matrix(0, nrow = 4, ncol = 6))
+
+# No shrinkage
+rec1 <- mip_l0(fc, S, W, G0 = NULL, 
+               lambda_0 = 0.1, lambda_1 = 0, lambda_2 = 0, M = NULL, 
+               solver = "gurobi")
+rec1$z
+rec1$G
+
+# Shrink toward G_bu
+rec2 <- mip_l0(fc, S, W, G0 = G_bu, 
+               lambda_0 = 0.1, lambda_1 = 0.1, lambda_2 = 0, M = NULL, 
+               solver = "gurobi")
+rec2$z
+rec2$G
+
+rec3 <- mip_l0(fc, S, W, G0 = G_bu, 
+               lambda_0 = 0.1, lambda_1 = 0, lambda_2 = 0.1, M = NULL, 
+               solver = "gurobi")
+rec3$z
+rec3$G
+
+# Shrink toward G_td
+rec4 <- mip_l0(fc, S, W, G0 = G_td, 
+               lambda_0 = 0.1, lambda_1 = 0.1, lambda_2 = 0, M = NULL, 
+               solver = "gurobi")
+rec4$z
+rec4$G
+
+rec5 <- mip_l0(fc, S, W, G0 = G_td, 
+               lambda_0 = 10, lambda_1 = 20, lambda_2 = 20, M = NULL, 
+               solver = "gurobi")
+rec5$z
+rec5$G
+
 
 
