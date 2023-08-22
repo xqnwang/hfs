0f1b1b9c6a9cd8013d5b438dc49fb231e0833c28
xqnwang <wang_study1995@163.com>
2023-08-23 01:40:35 +1000

    Include warmstart for intuitive method

--- Python/intuitive.py
+++ Python/intuitive.py
@@ -5,11 +5,12 @@ import gurobipy as gp
 from gurobipy import *
 # Gurobi Optimizer version 10.0.1 build v10.0.1rc0
 
-def miqp_AS(y, S, W, l0 = 0, m = None, MIPGap = None, TimeLimit = 600, LogToConsole = 0, OutputFlag = 0, MIPFocus = 0, Cuts = -1):
+def miqp_AS(y, S, W, l0 = 0, m = None, MIPGap = None, TimeLimit = 600, LogToConsole = 0, OutputFlag = 0, WarmStart = 1, MIPFocus = 0, Cuts = -1):
     """
-    Solve the OP problem: min_{A, G_bar} 0.5 * (y - S G_bar y)' W^{-1} (y - S G_bar y) + l0 * sum(A)
-                          s.t. G_bar = (S'A'W^{-1}AS)^{-1}S'A'W^{-1}
-                               G_bar S = I
+    Solve the OP problem: min_{A, G_bar, C} 0.5 * (y - S G_bar y)' W^{-1} (y - S G_bar y) + l0 * sum(A)
+                          s.t. C(S'A'W^{-1}AS) = I ...invertible
+                               G_bar = (S'A'W^{-1}AS)^{-1}S'A'W^{-1} ...G_bar
+                               G_bar S = I ...unbiasedness
 
     Parameters
     ----------
@@ -31,6 +32,8 @@ def miqp_AS(y, S, W, l0 = 0, m = None, MIPGap = None, TimeLimit = 600, LogToCons
         Enables or disables console logging. Use OutputFlag to shut off all logging.
     OutputFlag: int, optional
         Enables or disables solver output. Use LogFile and LogToConsole for finer-grain control. Setting OutputFlag to 0 is equivalent to setting LogFile to "" and LogToConsole to 0.
+    WarmStart: int, optional
+        bottom-up warm start. Default True.
     MIPFocus: int, optional
         If you are more interested in finding feasible solutions quickly, you can select MIPFocus=1. If you believe the solver is having no trouble finding good quality solutions, and wish to focus more attention on proving optimality, select MIPFocus=2. If the best objective bound is moving very slowly (or not at all), you may want to try MIPFocus=3 to focus on the bound.
     Cuts: int, optional
@@ -73,7 +76,7 @@ def miqp_AS(y, S, W, l0 = 0, m = None, MIPGap = None, TimeLimit = 600, LogToCons
     env.start()
     
     """ MODEL """
-    model = gp.Model('MIP_AS')
+    model = gp.Model('MIP_AS', env=env)
     
     """ PARAMETERS """
     ## A matrix
@@ -96,15 +99,28 @@ def miqp_AS(y, S, W, l0 = 0, m = None, MIPGap = None, TimeLimit = 600, LogToCons
     """ CONSTRAINTS """
     AUX = S.T @ A.T @ inv_W
     model.addConstr(E == y - S@G@y)
-    model.addConstr(C @ AUX == G) # invertible
+    model.addConstr(G @ A @ S == I) # Avoid C(S'A'W^{-1}AS) = I as Gurobi doesn't support multiplication of three MVars
+    model.addConstr(C @ AUX == G)
     model.addConstr(G @ S == I)
-    
-    #model.addConstr(E == y - S@G@y)
-    #model.addConstr(G@S == I)
-    #model.addConstr(S.T@A.T@inv_W@A@S@G == S.T@A.T@inv_W)
     model.update()
     
-    """ OPTIMIZE """    
+    """ WARM START """
+    if WarmStart:
+        model.NumStart = 2
+        model.update()
+        
+        # 1. Bottom-up
+        model.params.StartNumber = 0 # set StartNumber
+        rS = np.sum(S, axis=1)
+        z_bu = np.diag(np.where(rS == 1, 1, 0))
+        A.Start = z_bu
+        
+        # 2. All
+        model.params.StartNumber = 1
+        z_all = np.identity(n)
+        A.start = z_all
+    
+    """ OPTIMIZE """
     model.Params.MIPGap = MIPGap
     model.Params.OutputFlag = OutputFlag
     model.Params.LogToConsole = LogToConsole
