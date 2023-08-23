d6cccc041a24e5d721f4932ecd33981e3e43609c
xqnwang <wang_study1995@163.com>
2023-08-23 21:36:22 +1000

    Reconcile via the intuitive method

---
+++ R/intuitive_reconcile.R
@@ -0,0 +1,173 @@
+#' Intuitive method to achieve subset selection in forecast reconciliation
+#' 
+#' Reconcile forecasts in accordance with its key structure.
+#' 
+#' @param base_forecasts A vector or a matrix contains the base forecasts.
+#' @param S Summing or structural matrix.
+#' @param method Reconciliation method to use.
+#' @param residuals Historical residuals used to estimate the variance-covariance matrix of the h-step-ahead base forecast errors. It should not be `NULL` when method is one of `c("wls_var", "mint_cov", "mint_shrink")`.
+#' @param fitted_values Fitted values in training set.
+#' @param train_data Training data.
+#' @param subset Logical. If true, mixed integer programming is implemented to achieve subset selection.
+#' @param lambda_0 A user supplied `lambda_0` value for subset selection.
+#' @param nlambda Number of candidate `lambda_0` values to choose from for subset selection problem.
+#' @param m The value of the Big M for each element in G.
+#' @param MIPVerbose Logical. If true, enable console logging of MIP.
+#' @param SearchVerbose Logical. If true, a progress bar will be displayed when searching optimal `lambda_0`.
+#' 
+#' @import ROI
+#' @import future
+#' @import reticulate
+#' 
+#' @export
+intuitive.reconcile <- function(base_forecasts, S, 
+                                method = c("bu", "ols", "wls_struct", "wls_var", "mint_cov", "mint_shrink"), 
+                                residuals = NULL, fitted_values = NULL, train_data = NULL,
+                                subset = FALSE,
+                                lambda_0 = NULL, nlambda = 20,
+                                m = NULL, MIPGap = NULL, WarmStart = 1, MIPFocus = 0, Cuts = -1,
+                                TimeLimit = 600, MIPVerbose = FALSE, SearchVerbose = FALSE){
+  # Dimension info
+  n <- NROW(S); nb <- NCOL(S)
+  if (is.vector(base_forecasts)){
+    if (length(base_forecasts) != n){
+      stop("The dimensions of the base_forecasts do not match S")
+    }
+    base_forecasts <- as.matrix(base_forecasts, nrow = 1, ncol = n)
+  } else{
+    if(NCOL(base_forecasts) != n){
+      stop("The dimensions of the base_forecasts do not match S")
+    }
+  }
+  
+  method <- match.arg(method)
+  if (method == "bu"){
+    # Buttom-up
+    subset <- FALSE
+    G <- cbind(matrix(0, nb, n-nb), diag(nb))
+  } else{
+    # W matrix
+    if (method == "ols"){
+      W <- diag(n)
+    } else if (method == "wls_struct"){
+      W <- diag(rowSums(S))
+    } else{
+      # Unbiased sample covariance estimator
+      if (!is.null(residuals)){
+        Tt <- NROW(residuals)
+        covm <- crossprod(stats::na.omit(residuals)) / Tt
+      } else{
+        if (is.null(fitted_values) | is.null(train_data)){
+          stop("Historical residuals are required to estimate the variance-covariance matrix of the h-step-ahead base forecast errors")
+        } else{
+          residuals <- train_data - fitted_values
+          Tt <- NROW(residuals)
+          covm <- crossprod(stats::na.omit(residuals)) / Tt
+        }
+      }
+      
+      if (method == "wls_var"){
+        W <- diag(diag(covm))
+      } else if (method == "mint_cov"){
+        W <- covm
+      } else if (method == "mint_shrink"){
+        tar <- diag(apply(residuals, 2, compose(crossprod, stats::na.omit))/Tt)
+        corm <- cov2cor(covm)
+        xs <- scale(residuals, center = FALSE, scale = sqrt(diag(covm)))
+        xs <- xs[stats::complete.cases(xs),]
+        v <- (1/(Tt * (Tt - 1))) * (crossprod(xs^2) - 1/Tt * (crossprod(xs))^2)
+        diag(v) <- 0
+        corapn <- cov2cor(tar)
+        d <- (corm - corapn)^2
+        lambda <- sum(v)/sum(d)
+        lambda <- max(min(lambda, 1), 0)
+        W <- lambda * tar + (1 - lambda) * covm
+      } else {
+        stop("Unknown reconciliation method")
+      }
+    }
+    
+    # Check positive definiteness of weights
+    eigenvalues <- eigen(W, only.values = TRUE)[["values"]]
+    if (any(eigenvalues < 1e-8)) {
+      stop("MinT methods need covariance matrix to be positive definite.", call. = FALSE)
+    }
+    
+    # G matrix
+    R <- t(S) %*% solve(W)
+    G <- solve(R %*% S) %*% R
+    
+    # Group best-subset selection via intuitive method
+    if (subset){
+      # One-step ahead base forecasts
+      fc <- base_forecasts[1, ] |> matrix(nrow = n, ncol = 1)
+      fc_tilde_init <- S %*% G %*% fc
+      obj_init <- 0.5 * t(fc - fc_tilde_init) %*% solve(W) %*% (fc - fc_tilde_init) |> 
+        as.numeric()
+      
+      # Find optimal combination of lambda_0 and lambda_2 by minimizing sum of squared reconciled residuals
+      if (is.null(train_data) | is.null(fitted_values)){
+        stop("Training data and fitted values are required to find the optimal lambda_0")
+      }
+      
+      # Candidate lambda_0
+      if (is.null(lambda_0)){
+        lambda_0_max <- obj_init/nb
+        lambda_0_min <- 0.0001 * lambda_0_max
+        lambda_0 <- c(sapply(0:(nlambda-1), function(j) lambda_0_max*(lambda_0_min/lambda_0_max)^(j/(nlambda-1))), 0)
+      }
+      
+      # Search optimal l0
+      if (MIPVerbose){
+        LogToConsole = 1
+        OutputFlag = 1
+      } else{
+        LogToConsole = 0
+        OutputFlag = 0
+      }
+      
+      mip.out <- purrr::map(lambda_0, function(l0){
+        if (l0 == 0){
+          fit <- list(l0 = l0, G = G, Z = rep(1, n), obj = obj_init, gap = 0, opt = 1)
+        } else{
+          fit <- miqp_AS(fc, S, W, l0,
+                         m, MIPGap, TimeLimit, 
+                         LogToConsole, OutputFlag, 
+                         WarmStart, MIPFocus, Cuts)
+          names(fit) <- c("G", "Z", "obj", "gap", "opt")
+          fit <- append(list(l0 = l0), fit)
+        }
+        sse <- sum(stats::na.omit(train_data - fitted_values %*% t(fit$G) %*% t(S))^2)
+        fit$sse <- sse
+        return(fit)
+      }, .progress = SearchVerbose)
+      
+      sse_summary <- sapply(mip.out, function(l) c(l$l0, sum(as.vector(l$Z)), l$sse, l$obj, l$gap, l$opt)) |> 
+        t() |> 
+        data.frame()
+      names(sse_summary) <- c("lambda0", "k", "sse", "obj", "gap", "opt")
+      min.index <- purrr::map_dbl(mip.out, function(l) l$sse) |> round(2) |> which.min()
+      z <- mip.out[[min.index]]$Z |> as.vector()
+      G <- mip.out[[min.index]]$G
+    }
+  }
+  
+  # Reconciliation
+  y_tilde <- base_forecasts %*% t(G) %*% t(S)
+  colnames(y_tilde) <- colnames(base_forecasts)
+  
+  # Output
+  if (subset){
+    z <- z
+  } else{
+    z <- NA
+  }
+  if (exists("sse_summary")){
+    lambda_report <- sse_summary
+  } else{
+    lambda_report <- NA
+  }
+  list(y_tilde = y_tilde, G = G,
+       z = z,
+       lambda_report = lambda_report)
+}
---
+++ code/intuitive_reconciliation.R
@@ -0,0 +1,135 @@
+library(tidyverse)
+library(magrittr)
+library(future)
+library(forecast)
+
+# Setup
+data_label <- "simulation"
+# data_label <- "tourism"
+nlambda <- 20
+MonARCH <- TRUE
+workers <- parallel::detectCores()
+if (MonARCH){
+  path <- "~/.local/share/r-miniconda/envs/r-reticulate/bin/python3.8"
+  setwd(Sys.glob(file.path("~/wm15/", "*", "hfs")))
+} else{
+  path <- "~/Library/r-miniconda-arm64/bin/python3.10"
+}
+reticulate::use_python(path, required = T)
+reticulate::source_python("Python/intuitive.py")
+source("R/intuitive_reconcile.R")
+
+# Utility function
+reconcile_forecast <- function(index, fits, train, basefc, resids, test, S,
+                               method, method_name, nlambda,
+                               deteriorate = FALSE, deteriorate_series, deteriorate_rate,
+                               MIPFocus, Cuts, TimeLimit,
+                               MIPVerbose, SearchVerbose){
+  
+  n <- NCOL(fits)
+  fitted_values <- fits[fits$Index == index, -n] |> as.matrix()
+  residuals <- resids[resids$Index == index, -n] |> as.matrix()
+  train_data <- train[train$Index == index, -n] |> as.matrix()
+  base_forecasts <- basefc[basefc$Index == index, -n] |> as.matrix()
+  
+  if (deteriorate){
+    fitted_values[, deteriorate_series] <- fitted_values[, deteriorate_series]*deteriorate_rate
+    base_forecasts[, deteriorate_series] <- base_forecasts[, deteriorate_series]*deteriorate_rate
+    residuals <- train_data - fitted_values
+  }
+  
+  Base <- list(y_tilde = base_forecasts, G = NA, z = NA, lambda_report = NA)
+  BU <- intuitive.reconcile(base_forecasts = base_forecasts, S = S, method = "bu")
+  for(i in 1:length(method)){
+    assign(method_name[i], 
+           intuitive.reconcile(base_forecasts = base_forecasts, S = S, method = method[i], 
+                               residuals = residuals))
+    assign(paste0(method_name[i], "_intuitive"), 
+           intuitive.reconcile(base_forecasts = base_forecasts, S = S,
+                               method = method[i], residuals = residuals,
+                               fitted_values = fitted_values, train_data = train_data,
+                               subset = TRUE, nlambda = nlambda,
+                               MIPFocus = MIPFocus, Cuts = Cuts, TimeLimit = TimeLimit,
+                               MIPVerbose = MIPVerbose, SearchVerbose = SearchVerbose))
+    print(paste("===", method_name[i], "finished!"))
+  }
+  
+  mget(c("Base", "BU", method_name, paste0(method_name, "_intuitive")))
+}
+
+#################################################
+# Data
+#################################################
+#----------------------------------------------------------------------
+# Simulation data
+## Total/Middle/Bottom: 3 levels, n = 7
+## Training set:  1978Q1-2018Q4
+## Test set:      2019Q1-2022Q4
+#----------------------------------------------------------------------
+if (data_label == "simulation"){
+  MIPFocus = 0; Cuts = -1; TimeLimit = 600; MIPVerbose = FALSE; SearchVerbose = FALSE
+  method <- c("ols", "wls_struct", "wls_var", "mint_cov", "mint_shrink")
+  method_name <- c("OLS", "WLSs", "WLSv", "MinT", "MinTs")
+}
+
+#----------------------------------------------------------------------
+# Australian domestic tourism (only considering hierarchical structure)
+##
+## Monthly series from 1998Jan-2017Dec: 240 months (20 years) for each series
+##
+## Total/State/Zone/Region: 4 levels, n = 111 series in total
+##
+## Training set:  1998Jan-2016Dec
+## Test set:      2017Jan-2017Dec
+#----------------------------------------------------------------------
+if (data_label == "tourism"){
+  MIPFocus = 3; Cuts = 2; TimeLimit = 600; MIPVerbose = FALSE; SearchVerbose = TRUE
+  method <- c("ols", "wls_struct", "wls_var", "mint_shrink")
+  method_name <- c("OLS", "WLSs", "WLSv", "MinTs")
+}
+
+#################################################
+# Import base forecast results
+#################################################
+for (i in c("S", "fits", "resids", "train", "basefc","test")){
+  assign(i, readRDS(file = paste0("data/", data_label, "_", i, ".rds")))
+}
+indices <- unique(fits$Index)
+
+#################################################
+# Reconcile forecasts
+#################################################
+reconsf <- indices |>
+  purrr::map(\(index) reconcile_forecast(index, fits, train, basefc, resids, test, S, 
+                                         method, method_name, nlambda,
+                                         deteriorate = FALSE, 
+                                         MIPFocus = MIPFocus, Cuts = Cuts, TimeLimit = TimeLimit,
+                                         MIPVerbose = MIPVerbose, SearchVerbose = SearchVerbose),
+             .progress = !SearchVerbose)
+saveRDS(reconsf, file = paste0("data_new/", data_label, "_intuitive_reconsf.rds"))
+rm(reconsf)
+
+#################################################
+# Reconcile forecasts - deteriorate base forecasts for some time series
+#################################################
+if (data_label == "simulation"){
+  scenario <- c("s1", "s2", "s3")
+  deteriorate_series <- c("AA", "A", "Total")
+  deteriorate_rate <- rep(1.5, 3)
+  for (i in 1:3){
+    reconsf_s <- indices |>
+      purrr::map(\(index) reconcile_forecast(index, fits, train, basefc, resids, test, S,
+                                             method, method_name, nlambda,
+                                             deteriorate = TRUE, 
+                                             deteriorate_series = deteriorate_series[i],
+                                             deteriorate_rate = deteriorate_rate[i], 
+                                             MIPFocus = MIPFocus, Cuts = Cuts, TimeLimit = TimeLimit,
+                                             MIPVerbose = MIPVerbose, SearchVerbose = SearchVerbose),
+                 .progress = !SearchVerbose)
+    saveRDS(reconsf_s, file = paste0("data_new/", data_label, "_intuitive_reconsf_", scenario[i], ".rds"))
+    rm(reconsf_s)
+    print(paste("Scenario", i, "finished!"))
+  }
+}
+
+
